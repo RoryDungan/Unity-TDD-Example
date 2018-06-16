26cb5add4ea3637063b25db3f26d713660d22b01
Rory Dungan <rorydungan@gmail.com>
2018-06-16 14:10:29 +1000

    Add wrapper for paddle bounce surface

---
+++ GameDLL/Game/Unity/PaddleBounceSurface.cs
@@ -0,0 +1,21 @@
+﻿using Game.BreakoutGame;
+using Game.UnityWrapper;
+using UnityEngine;
+
+namespace Game.Unity
+{
+    class PaddleBounceSurface : MonoBehaviour, IBounceSurface
+    {
+        BreakoutGame.PaddleBounceSurface bounceSurface;
+
+        void Awake()
+        {
+            bounceSurface = new BreakoutGame.PaddleBounceSurface(transform.Wrap());
+        }
+
+        public Vector3 Bounce(Vector3 hitPosition, Vector3 hitDirection, Vector3 surfaceNormal)
+        {
+            return bounceSurface.Bounce(hitPosition, hitDirection, surfaceNormal);
+        }
+    }
+}
