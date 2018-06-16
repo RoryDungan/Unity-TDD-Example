4ed6eee70ca5ee84658794ca741e6eada9f7e968
Rory Dungan <rorydungan@gmail.com>
2018-06-16 16:11:56 +1000

    Set up script to update UI

---
+++ GameDLL/Game/Unity/ScoreUI.cs
@@ -0,0 +1,45 @@
+﻿using Game.BreakoutGame;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace Game.Unity
+{
+    public class ScoreUI : MonoBehaviour
+    {
+        private Text scoreText;
+        private IGameManager gameManager;
+
+        void Awake()
+        {
+            scoreText = GetComponentInChildren<Text>();
+            Assert.IsNotNull(
+                scoreText, 
+                "ScoreUI could not find Text component in children."
+            );
+
+            gameManager = GameManagerSingleton.Instance;
+            gameManager.OnScoreChanged += GameManager_OnScoreChanged;
+        }
+
+        void OnDestroy()
+        {
+            gameManager.OnScoreChanged -= GameManager_OnScoreChanged;
+        }
+
+        private void GameManager_OnScoreChanged(object sender, ScoreChangedEventArgs e)
+        {
+            UpdateScore(e.NewScore);
+        }
+
+        void Start()
+        {
+            UpdateScore(gameManager.Score);
+        }
+
+        void UpdateScore(int newScore)
+        {
+            scoreText.text = newScore.ToString();
+        }
+    }
+}
