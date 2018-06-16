f2b7b7e88da2dd6994e0ae787c7fc3db8a5e6b06
Rory Dungan <rorydungan@gmail.com>
2018-06-16 18:14:57 +1000

    Set up first integration tests inside Unity

---
+++ Assets/Code/IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,19 @@
+﻿using Game.Unity;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class IntegrationTests 
+{
+    [UnityTest]
+    public IEnumerator ball_bounces_off_paddle()
+    {
+        var paddle = new GameObject();
+        paddle.AddComponent<PaddleBounceSurface>();
+
+        var ball = new GameObject();
+        ball.AddComponent<Ball>();
+
+        yield return new WaitForEndOfFrame();
+    }
+}
