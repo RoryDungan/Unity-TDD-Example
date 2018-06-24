1e0b74e8730bf2c91428a438f707417da3c1183c
Rory Dungan <rorydungan@gmail.com>
2018-06-24 19:24:04 +1000

    Write another example integration test

--- Assets/Code/IntegrationTests/IntegrationTests.cs
+++ Assets/Code/IntegrationTests/IntegrationTests.cs
@@ -6,7 +6,7 @@ using UnityEngine.TestTools;
 using UnityEngine.TestTools.Utils;
 using GameManagerSingleton = Game.BreakoutGame.GameManagerSingleton;
 
-public class IntegrationTests 
+public class IntegrationTests
 {
     /// <summary>
     /// Helper function to wait for the specified fixed update time.
@@ -50,12 +50,12 @@ public class IntegrationTests
         try
         {
             yield return WaitForFixedTime(1f);
-            
+
             // Allow for 1 frame of error.
             var equalityComparer = new Vector3EqualityComparer(0.01f);
 
             Assert.That(
-                ball.transform.position, 
+                ball.transform.position,
                 Is.EqualTo(new Vector3(0f, 3f)).Using(equalityComparer)
             );
         }
@@ -88,7 +88,46 @@ public class IntegrationTests
         {
             Object.Destroy(ball);
             Object.Destroy(block);
-
         }
+    }
+
+    [UnityTest]
+    public IEnumerator block_smashes_on_touch()
+    {
+        // Arrange
+        var block = new GameObject("Block");
+        block.AddComponent<DefaultBounceSurface>();
+        block.AddComponent<BoxCollider2D>();
+        block.AddComponent<Block>();
+
+        block.transform.position = new Vector3(0f, 3f, 0f);
+
+        var ball = new GameObject("Ball");
+        ball.AddComponent<Ball>();
+        ball.AddComponent<CircleCollider2D>();
+        var ballRb = ball.AddComponent<Rigidbody2D>();
+        ballRb.bodyType = RigidbodyType2D.Kinematic;
+        ballRb.useFullKinematicContacts = true;
+        ball.transform.position = Vector3.zero;
+
+        try
+        {
+            // Act
+            yield return new WaitForSeconds(1f);
+
+            // Assert
+            Assert.True(block == null);
+        }
+        finally
+        {
+            // cleanup
+            if (block)
+            {
+                Object.Destroy(block);
+            }
+            Object.Destroy(ball);
+        }
+
+        yield return null;
     }
 }
